2c8ed44dbc16bde8d1e55ac326d4f90c4289b964
Woojin Kim <woojin941103@gmail.com>
2017-08-10 19:20:15 +0900

    탭바 컨트롤러, 메인페이지 추가

---
+++ Gachi/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  Gachi
+//
+//  Created by Woojin Kim on 2017. 8. 5..
+//  Copyright © 2017년 Woojin Kim. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+class MainViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
